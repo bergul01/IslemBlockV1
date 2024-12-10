d59be73637cf5c381fc9b99ce51cdf0e2aeeaa6e
Berkay <gulberkay@hotmail.com>
2024-12-11 01:52:24 +0300

    IslemBlockV1

---
+++ IslemBlok/src/main/java/IslemBlok/IslemBlok/entity/User.java
@@ -0,0 +1,40 @@
+package IslemBlok.IslemBlok.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_name")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "mail")
+	private String email;
+	
+	
+	
+	
+	
+	
+}
